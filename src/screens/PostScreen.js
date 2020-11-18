ec09b9845d6d94a1dd5a61277ce94692f7dc43b1
Serg <tumanovser@mail.ru>
2020-11-18 06:05:36 +0300

    73

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   StyleSheet,
   Text,
@@ -10,11 +10,16 @@ import {
 } from "react-native";
 import { DATA } from "../data";
 import { THEME } from "../theme";
+import { Item, HeaderButtons } from "react-navigation-header-buttons";
+import { AppHeaderIcon } from "../components/AppHeaderIcon";
 
 export const PostScreen = ({ navigation }) => {
   const postId = navigation.getParam("postId");
 
   const post = DATA.find((p) => p.id === postId);
+  // useEffect(() => {
+  //   navigation.setParams({ booked: post.booked });
+  // }, []);
 
   const removeHandler = () => {
     Alert.alert(
@@ -48,8 +53,20 @@ export const PostScreen = ({ navigation }) => {
 
 PostScreen.navigationOptions = ({ navigation }) => {
   const date = navigation.getParam("date");
+  const booked = navigation.getParam("booked");
+  const iconName = booked ? "ios-star" : "ios-star-outline";
+
   return {
     headerTitle: "Пост от " + new Date(date).toLocaleDateString(),
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+        <Item
+          title="Take"
+          iconName={iconName}
+          onPress={() => console.log("Press foto")}
+        />
+      </HeaderButtons>
+    ),
   };
 };
 
